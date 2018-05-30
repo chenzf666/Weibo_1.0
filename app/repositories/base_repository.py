0bd004a248041c91f004ae13de4734f4bbd83a07
CantonHannibal <31664020+CantonHannibal@users.noreply.github.com>
2018-05-30 13:41:04 +0800

    Update base_repository.py

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
-
+from  werkzeug.exceptions import Aborter
 class BaseRepository:
     __metaclass__ = ABCMeta
 
@@ -37,3 +37,50 @@ class BaseRepository:
     @abstractmethod
     def all(self):
         pass
+
+
+class SQLAlchemyRepository(BaseRepository):
+
+    def __init__(self, model, session):
+        self.model = model
+        self.session = session
+
+
+
+
+    def get(self, **pk):
+        return self.session.query(self.model).filter_by(**pk).one()
+
+    def get_or_404(self, **keys):
+        rv = self.session.query(self.model).filter_by(**keys).one()
+        if rv is None:
+            Aborter.abort(404)
+        return rv
+
+    # find all models filtered by some conditions
+    def find(self, **keys):
+        self.session.query(self.model).filter_by(**keys).first()
+
+
+    def add(self, entity):
+        self.session.add(entity)
+        self.session.commit()
+
+
+    def add_all(self, entities):
+        self.session.add(entities)
+        self.session.commit()
+
+
+    def delete(self, entity):
+        self.session.delete(entity)
+        self.session.commit()
+
+    def create(self, **kw):
+        return self.model(**kw)
+
+
+    def all(self):
+        return list(self.session.query(self.model).all())
+
+
